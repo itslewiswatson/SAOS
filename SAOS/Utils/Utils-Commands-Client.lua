5c6981e0866c1ffc9dcf64043a4ac673d3162a18
Callum Dawson <Callum5829@gmail.com>
2015-02-10 17:38:08 +0000

    Added 'clearchat' command and small optimisation

--- SAOS/Utils/Utils-Commands-Client.lua
+++ SAOS/Utils/Utils-Commands-Client.lua
@@ -1,3 +1,9 @@
-addCommandHandler("language",function(source)
+addCommandHandler("language",function()
 	outputChatBox("* Your language is currently set to "..getLocalization()["name"]..".",0,255,0)
+end,false,false)
+
+addCommandHandler("clearchat",function()
+	for i=1,getChatboxLayout()["chat_lines"] do
+		outputChatBox("")
+	end
 end,false,false)
