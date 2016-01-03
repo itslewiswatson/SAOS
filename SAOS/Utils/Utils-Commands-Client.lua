aef3a32514141bfbd8e3ef881d425547c7bcf938
Callum Dawson <callum@cldsystems.co.uk>
2016-01-03 06:23:32 +0000

    Added 'getpos' command for development purposes

--- SAOS/Utils/Utils-Commands-Client.lua
+++ SAOS/Utils/Utils-Commands-Client.lua
@@ -1,9 +1,15 @@
 addCommandHandler("language",function()
 	outputChatBox("* Your language is currently set to "..getLocalization()["name"]..".",0,255,0)
-end,false,false)
+end,false)
 
 addCommandHandler("clearchat",function()
 	for i=1,getChatboxLayout()["chat_lines"] do
 		outputChatBox("")
 	end
-end,false,false)
+end,false)
+
+addCommandHandler("getpos",function()
+	local pos = localPlayer:getPosition()
+	outputChatBox("* Position: "..tostring(pos.x)..", "..tostring(pos.y)..", "..tostring(pos.z),153,50,204)
+	setClipboard(tostring(pos.x)..","..tostring(pos.y)..","..tostring(pos.z))
+end,false)
